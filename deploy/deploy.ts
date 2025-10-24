0af81f513841c06b8c619364306893c5267a9562
mikanbytes <hkgrtv77855@outlook.com>
2025-10-24 20:45:40 +0800

    update deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedCipherArena = await deploy("CipherArena", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`CipherArena contract: `, deployedCipherArena.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_cipherArena"; // id required to prevent reexecution
+func.tags = ["CipherArena"];
